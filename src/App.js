bbf196a4f3c1c5dc026d20000d08387ad8074f6d
Vinicius Silva Souza <viniciusvss120@gmail.com>
2023-01-22 11:38:15 -0400

    projeto portifolio

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href='#'>Vinicius <span className='lestName'>Silva Souza</span></a>
+        <nav>
+          <ul>
+            <li><a href='#'></a>Home</li>
+            <li><a href='#'></a>About</li>
+            <li><a href='#'></a>Project</li>
+            <li><a href='#'></a>Contato</li>
+          </ul>
+          <div className='button'>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
